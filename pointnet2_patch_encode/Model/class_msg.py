ca8e8ab29f94bcab93df639de72995b039492a62
chLi <565353780@qq.com>
2022-11-09 18:01:04 +0800

    simplify class msg into autoencoder

--- pointnet2_patch_encode/Model/class_msg.py
+++ pointnet2_patch_encode/Model/class_msg.py
@@ -4,49 +4,23 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-from pointnet2_patch_encode.Model.utils.set_abstraction import SetAbstraction
-from pointnet2_patch_encode.Model.utils.set_abstraction_msg import SetAbstractionMsg
+from pointnet2_patch_encode.Model.encoder import Encoder
+from pointnet2_patch_encode.Model.resnet_decoder import ResNetDecoder
 
 
 class ClassMsg(nn.Module):
 
-    def __init__(self, num_class, normal_channel=True):
+    def __init__(self, feature_size=1024, normal_channel=False):
         super(ClassMsg, self).__init__()
-        in_channel = 3 if normal_channel else 0
-        self.normal_channel = normal_channel
-        self.sa1 = SetAbstractionMsg(
-            512, [0.1, 0.2, 0.4], [16, 32, 128], in_channel,
-            [[32, 32, 64], [64, 64, 128], [64, 96, 128]])
-        self.sa2 = SetAbstractionMsg(
-            128, [0.2, 0.4, 0.8], [32, 64, 128], 320,
-            [[64, 64, 128], [128, 128, 256], [128, 128, 256]])
-        self.sa3 = SetAbstraction(None, None, None, 640 + 3, [256, 512, 1024],
-                                  True)
-        self.fc1 = nn.Linear(1024, 512)
-        self.bn1 = nn.BatchNorm1d(512)
-        self.drop1 = nn.Dropout(0.4)
-        self.fc2 = nn.Linear(512, 256)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.drop2 = nn.Dropout(0.5)
-        self.fc3 = nn.Linear(256, num_class)
+        self.feature_size = feature_size
+
+        self.encoder = Encoder(feature_size, normal_channel)
+        self.decoder = ResNetDecoder(feature_size, relu_in=True)
         return
 
     def forward(self, xyz):
-        B, _, _ = xyz.shape
-        if self.normal_channel:
-            norm = xyz[:, 3:, :]
-            xyz = xyz[:, :3, :]
-        else:
-            norm = None
-
-        l1_xyz, l1_points = self.sa1(xyz, norm)
-        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
-        _, l3_points = self.sa3(l2_xyz, l2_points)
-        x = l3_points.view(B, 1024)
-        x = self.drop1(F.relu(self.bn1(self.fc1(x))))
-        x = self.drop2(F.relu(self.bn2(self.fc2(x))))
-        x = self.fc3(x)
-        x = F.log_softmax(x, -1)
+        l3_points = self.encoder(xyz)
+        x = self.decoder(l3_points)
         return x, l3_points
 
 
