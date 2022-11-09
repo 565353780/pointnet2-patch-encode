52d108065c15d93e351c1397659c0e56e4c1c6b8
chLi <565353780@qq.com>
2022-11-09 17:05:54 +0800

    finish basic detector module

--- pointnet2_patch_encode/Module/detector.py
+++ pointnet2_patch_encode/Module/detector.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pointnet2_patch_encode.Model.
+import torch
+import numpy as np
+
+from pointnet2_patch_encode.Model.class_msg import ClassMsg
+
 
 class Detector(object):
+
     def __init__(self):
+        self.model = ClassMsg(936, False).cuda()
+        self.model.eval()
         return
+
+    def detect(self, point_array):
+        inputs = torch.tensor(point_array.astype(np.float32)).transpose(
+            2, 1).cuda()
+        pred, trans_feat = self.model(inputs)
+        return pred, trans_feat
