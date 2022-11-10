83d4196688ed3c36ccef1c9eec67992ea9e50f0d
chLi <565353780@qq.com>
2022-11-10 20:55:28 +0800

    update setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,3 +1,25 @@
+cd ..
+git clone git@github.com:565353780/mesh-manage.git
+git clone git@github.com:565353780/udf-generate.git
+git clone git@github.com:565353780/scannet-dataset-manage.git
+git clone git@github.com:565353780/scan2cad-dataset-manage.git
+git clone git@github.com:565353780/shapenet-dataset-manage.git
+
+cd mesh-manage
+./dev_setup.sh
+
+cd ../udf-generate
+./dev_setup.sh
+
+cd ../scannet-dataset-manage
+./dev_setup.sh
+
+cd ../scan2cad-dataset-manage
+./dev_setup.sh
+
+cd ../shapenet-dataset-manage
+./dev_setup.sh
+
 pip install open3d numpy pickle tqdm
 pip3 install torch torchvision torchaudio \
   --extra-index-url https://download.pytorch.org/whl/cu113
--- setup.sh
+++ setup.sh
@@ -1,3 +1,25 @@
+cd ..
+git clone https://github.com/565353780/mesh-manage.git
+git clone https://github.com/565353780/udf-generate.git
+git clone https://github.com/565353780/scannet-dataset-manage.git
+git clone https://github.com/565353780/scan2cad-dataset-manage.git
+git clone https://github.com/565353780/shapenet-dataset-manage.git
+
+cd mesh-manage
+./setup.sh
+
+cd ../udf-generate
+./setup.sh
+
+cd ../scannet-dataset-manage
+./setup.sh
+
+cd ../scan2cad-dataset-manage
+./setup.sh
+
+cd ../shapenet-dataset-manage
+./setup.sh
+
 pip install open3d numpy pickle tqdm
 pip3 install torch torchvision torchaudio \
   --extra-index-url https://download.pytorch.org/whl/cu113
