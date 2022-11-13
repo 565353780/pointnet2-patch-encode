c3de36e928bd43fc1297d8def612030392b1adb6
chLi <565353780@qq.com>
2022-11-13 20:21:45 +0800

    use random occ to train

--- pointnet2_patch_encode/Dataset/simple_occ_dataset.py
+++ pointnet2_patch_encode/Dataset/simple_occ_dataset.py
@@ -11,31 +11,24 @@ class SimpleOccDataset(Dataset):
     def __init__(self, crop_num=32):
         self.crop_num = crop_num
         self.total_crop_num = self.crop_num**3
+        self.dataset_len = 100000
         return
 
     def __getitem__(self, index, test=False):
-        assert index < 2**self.total_crop_num
+        assert index < self.dataset_len
 
         data = {'inputs': {}, 'predictions': {}, 'losses': {}, 'logs': {}}
 
-        bin_str = bin(index)[2:]
-        if len(bin_str) < self.total_crop_num:
-            append_zero_num = self.total_crop_num - len(bin_str)
-            bin_str = '0' * append_zero_num + bin_str
-
-        occ = np.array([int(single_bin_str) for single_bin_str in bin_str],
-                       dtype=int)
+        occ = np.random.randint(2,
+                                size=(1, self.crop_num, self.crop_num,
+                                      self.crop_num))
         if test:
             data['inputs']['occ'] = torch.tensor(
                 occ.reshape(1, 1, self.crop_num, self.crop_num,
                             self.crop_num).astype(np.float32))
         else:
-            data['inputs']['occ'] = torch.tensor(
-                occ.reshape(1, self.crop_num, self.crop_num,
-                            self.crop_num).astype(np.float32))
+            data['inputs']['occ'] = torch.tensor(occ.astype(np.float32))
         return data
 
     def __len__(self):
-        # FIXME: dataset is too large to training
-        return int(1e10)
-        return 2**self.total_crop_num
+        return self.dataset_len
